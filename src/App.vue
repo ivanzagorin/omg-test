df99cb7db5ab5f87a3e0a74aa38f7ae55acef3e6
izagorin <izagorin@webpower.ru>
2024-04-30 19:35:57 +0500

    project-settings: setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from '@/components/Header'
+import List from '@/components/List';
+</script>
+
+<template>
+  <Header />
+
+  <main>
+    <List />
+  </main>
+</template>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <div class="header-title">
+      OMG test
+    </div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/images/logo.svg"
+      width="30"
+      height="30"
+    />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import './style';
+</style>
